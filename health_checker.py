2d899e6864f7cd706467d71c92853ebb7c4f34c5
Auroshis Ray <47098039+Auroshis@users.noreply.github.com>
2025-04-10 16:59:50 +0530

    Create health_checker.py

---
+++ health_checker.py
@@ -0,0 +1,21 @@
+import threading
+import time
+import requests
+
+class HealthChecker(threading.Thread):
+    def __init__(self, servers, balancer, interval=10):
+        super().__init__()
+        self.servers = servers
+        self.balancer = balancer
+        self.interval = interval
+        self.daemon = True # Ensures thread exits with main program
+
+    def run(self):
+        while True:
+            for server in self.servers:
+                try:
+                    r = requests.get(f"http://{server}", timeout=2)
+                    self.balancer.update_health(server, r.status_code == 200)
+                except:
+                    self.balancer.update_health(server, False)
+            time.sleep(self.interval)
