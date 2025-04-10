84c96302bba8f44dd51f8304479221c946ba253f
Auroshis Ray <47098039+Auroshis@users.noreply.github.com>
2025-04-10 22:35:37 +0530

    Update main.py - change http client to aiohttp

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import uuid
+from aiohttp import web, ClientSession
 from balancer import LoadBalancer
 from health_checker import HealthChecker
 
-SERVERS = ["localhost:8001", "localhost:8002", "localhost:8003"]
+SERVERS = ["localhost:8001", "localhost:8002"]
 
 balancer = LoadBalancer(SERVERS)
 checker = HealthChecker(SERVERS, balancer)
 checker.start()
 
-class ProxyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        try:
-            target = balancer.get_server_round_robin()
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(f"Forwarding to: {target}".encode())
-        except Exception as e:
-            self.send_response(503)
-            self.end_headers()
-            self.wfile.write(f"Error: {e}".encode())
+async def handle_request(request: web.Request):
+    try:
+        target = balancer.get_server_round_robin()
+        trace_id = str(uuid.uuid4())
+        path = request.rel_url
+        url = f"http://{target}{path}"
 
-def run():
-    server = HTTPServer(('0.0.0.0', 8080), ProxyHandler)
-    print("Load Balancer running on port 8080")
-    server.serve_forever()
+        headers = dict(request.headers)
+        headers["X-Trace-ID"] = trace_id
+
+        body = await request.read()
+
+        async with ClientSession() as session:
+            async with session.request(
+                method=request.method,
+                url=url,
+                headers=headers,
+                data=body,
+                allow_redirects=False,
+                timeout=5
+            ) as resp:
+                response_body = await resp.read()
+
+                # Build response
+                return web.Response(
+                    status=resp.status,
+                    body=response_body,
+                    headers=resp.headers
+                )
+
+    except Exception as e:
+        return web.Response(status=503, text=f"Error: {e}")
+
+app = web.Application()
+app.router.add_route("*", "/{tail:.*}", handle_request)
 
 if __name__ == "__main__":
-    run()
+    web.run_app(app, port=8080)
